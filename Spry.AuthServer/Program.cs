35b1ae54f7ab6e841310271cde607bca704c3ffc
Kwaku Otuo <kwaku.otuo@petraonline.com>
2024-08-07 08:34:49 +0000

    use https

--- Spry.AuthServer/Program.cs
+++ Spry.AuthServer/Program.cs
@@ -49,7 +49,7 @@ namespace Spry.AuthServer
                 app.UseHsts();
             }
 
-            //app.UseHttpsRedirection();
+            app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
